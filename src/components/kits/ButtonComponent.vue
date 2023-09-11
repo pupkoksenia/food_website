c5a26b7029dfe2141ca9ca3a09b300f396bed332
Kseniya <pupkoksenia@gmail.com>
2023-09-11 15:25:29 +0200

    auth

---
+++ src/components/kits/ButtonComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <button :class="type" class="small" style="width: 184px; height: 46px">
+      <strong style="width: max-content">{{ label }}</strong>
+    </button>
+  </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ButtonComponent',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+$font-color-hover: #f03785;
+$font-color: #cb3171;
+$font-color-active: #952252;
+
+button {
+  background: $font-color;
+  border-radius: 10px;
+  border: 2px solid $font-color;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 16px;
+  transition: all 0.2s ease;
+}
+button:hover {
+  background: $font-color-hover;
+  border: 2px solid $font-color-hover;
+}
+button:active {
+  background: $font-color-active;
+  border: 2px solid $font-color-active;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { createPinia } from 'pinia'
 import router from './router'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
-import { useFirebaseConfig } from '../src/stores/firebaseConfig'
-
-const firebaseConfigStore = useFirebaseConfig()
-const firebaseApp = initializeApp(firebaseConfigStore.firebaseConfig)
-export const db = getFirestore(firebaseApp)
+import { useFirebaseConfigStore } from '../src/stores/firebaseConfig'
+import './sass/app.scss'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 const pinia = createPinia()
+
 createApp(App).use(router).use(pinia).mount('#app')
+
+const firebaseConfigStore = useFirebaseConfigStore()
+const firebaseApp = initializeApp(firebaseConfigStore.firebaseConfig)
+export const db = getFirestore(firebaseApp)
